c852d6e33ba81179c76ee2f26ca8c5429982e9f9
Gurvinder Singh <ginder251@gmail.com>
2026-06-25 11:53:34 +0530

    Machine Discovery Service: Initial commit
    Signed-off-by: Gurvinder Singh <ginder251@gmail.com>

---
+++ machine-discovery-service/src/main/java/com/vms/machine_discovery/MachineDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.vms.machine_discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachineDiscoveryApplication.class, args);
+	}
+
+}
---
+++ machine-discovery-service/src/test/java/com/vms/machine_discovery/MachineDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vms.machine_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachineDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
